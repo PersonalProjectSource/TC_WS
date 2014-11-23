0f3a5f8b575851579e873f5b8a78c61ea996235a
root <laurent.brau@gmail.com>
2014-11-23 23:00:47 +0100

    first commit

---
+++ src/TC/TennisBundle/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TC\TennisBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('role')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TC\TennisBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'tc_tennisbundle_user';
+    }
+}
---
+++ src/TC/TennisBundle/TCTennisBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TC\TennisBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TCTennisBundle extends Bundle
+{
+}

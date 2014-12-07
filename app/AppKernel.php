702999579eac6c8c3c2b751ff8b3384620987372
root <laurent.brau@gmail.com>
2014-12-07 23:19:06 +0100

    {LB} ajout du bundle front

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new TC\TennisBundle\TCTennisBundle(),
+            new TC\FrontBundle\TCFrontBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/TC/FrontBundle/TCFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TC\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TCFrontBundle extends Bundle
+{
+}

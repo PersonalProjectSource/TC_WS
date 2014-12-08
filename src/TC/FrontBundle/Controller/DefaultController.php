f1c722bb9beff6f633c4246c9c5bc5cdef891863
optimusThePrime <laurent.brau@gmail.com>
2014-12-08 13:37:49 +0100

    securisation mdp

--- src/TC/FrontBundle/Controller/DefaultController.php
+++ src/TC/FrontBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array('name' => "coucou");
+        return array('name' => "");
     }
 }
